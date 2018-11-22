3e5cc3320cf189af3ab794702eb7527a330b3e5c
sura <sura907@hotmail.com>
2018-11-22 16:31:16 +0800

    split the routes

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 /**
  * 封装koa及其中间件供server使用
  */
-
+// 引入框架及中间件
 const Koa = require('koa')
+const router = require('koa-router')()
+
+// 引入子路由模块
+const ownRoutes = require('./routes/index')
+
+
 const app = new Koa()
 
-// test
-app.use( async (ctx) => {
-  ctx.body = 'hello, world'
-})
+// 使用路由分割用户请求
+router.use(ownRoutes)
+
+// 装载中间件
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 // 暴露Koa实例
 module.exports = app.callback()
