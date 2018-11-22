01441edd99042542c3ef95a6aa1a926b85e04b09
sura <sura907@hotmail.com>
2018-11-22 16:32:44 +0800

    centralized routing

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+/**
+ * 路由模块
+ *  切割请求
+ */
+const router = require('koa-router')()
+
+// 引入子路由
+const index = require('./default/index')
+
+
+// 转载子路由
+router.use('/', index)
+
+module.exports = router.routes()
