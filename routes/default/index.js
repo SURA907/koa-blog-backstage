3cde2f2ecc5d2e877ec877fb305b3026a7467255
sura <sura907@hotmail.com>
2018-11-22 23:49:22 +0800

    test database model

--- routes/default/index.js
+++ routes/default/index.js
@@ -4,9 +4,18 @@
  */
 const router = require('koa-router')()
 
+// 引入user model
+const articles = require('./../../model/model').getModel('articles')
+
 // test
-router.get('/', (ctx) => {
-  ctx.response.body = 'index test'
+router.get('/', async (ctx) => {
+  let index_data = await articles.find({is_delete:'NO'})
+  ctx.body = {
+    code: 0,
+    status: 200,
+    message: null,
+    data: index_data
+  }
 })
 
 module.exports = router.routes()
