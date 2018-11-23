9ea83a632fe15b47970badd6ebd6583e0f53b562
sura <sura907@hotmail.com>
2018-11-23 13:47:31 +0800

    test throw http error

--- routes/default/index.js
+++ routes/default/index.js
@@ -9,13 +9,15 @@ const articles = require('./../../model/model').getModel('articles')
 
 // test
 router.get('/', async (ctx) => {
-  let index_data = await articles.find({is_delete:'NO'})
-  ctx.body = {
-    code: 0,
-    status: 200,
-    message: null,
-    data: index_data
-  }
+  // let index_data = await articles.find({is_delete:'NO'})
+  // ctx.body = {
+  //   code: 0,
+  //   status: 200,
+  //   message: null,
+  //   data: index_data
+  // }
+  
+  // ctx.throw(404)
 })
 
 module.exports = router.routes()
