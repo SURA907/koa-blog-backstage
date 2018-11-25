ebd42949e19b2f68ecd9b9d7b144f10bfdbef169
sura <sura907@hotmail.com>
2018-11-25 16:50:28 +0800

    nothing change

--- config.js
+++ config.js
@@ -3,11 +3,16 @@
  *  DB: mongodb数据库
  *  HTTP_PORT: http端口
  *  HTTPS_PORT: https端口
+ *  CA_CERT: https使用的ca证书
  */
-const config = {
+const configs = {
+  DB: 'mongodb://localhost/test',
   HTTP_PORT: 8080,
   HTTPS_PORT: 443,
-  DB: 'mongodb://localhost/test',
+  CA_CERT: {
+    KEY: '',
+    CERT: ''
+  }
 }
 
-module.exports = config
+module.exports = configs
