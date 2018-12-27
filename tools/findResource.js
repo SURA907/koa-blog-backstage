5cc0b8d317fe9dff1cee990e638bcab62817986a
sura <sura907@hotmail.com>
2018-12-27 15:18:35 +0800

    change the findArticle to findResource

---
+++ tools/findResource.js
@@ -0,0 +1,121 @@
+/**
+ * 处理查找资源的请求
+ *   在本模块应用redis缓存策略
+ * 入口接收的参数说明：
+ * ctx: koa ctx对象
+ * next: koa next对象
+ * id: 资源唯一标识符
+ * flag: 资源种类标识符（article、user等）
+ * model: 存储资源的数据库model
+ * redis_client: 存储资源的redis
+ */
+
+// 延时
+async function sleep(ms) {
+  await new Promise( resolve => {
+    setTimeout(resolve, ms)
+  })
+}
+
+// 查询redis，查看数据的状态
+async function query_redis(redis_client, key) {
+  let result = await redis_client.getAsync(key)
+  return result
+}
+
+// 刷新资源过期时间（五分钟）
+async function reset_redis(redis_client, key) {
+  await redis_client.pexpireAsync(key, 300000)
+}
+
+// redis上锁
+async function redis_lock(redis_client, lock_key, lock_id) {
+  // 上锁
+  // 参数'NX'保证一次最多只有一个线程set成功
+  let is_lock = await redis_client.setAsync(lock_key, lock_id, 'NX', 'PX', 200)
+  return is_lock === 'OK'
+}
+
+// 查询mongo
+async function query_mongo(model, id) {
+  if (model.findById) {
+    let result = await model.findById(id)
+    return result
+  }
+}
+
+// 解锁并刷新redis资源数据
+async function redis_unlock(redis_client, lock_key, lock_id, key, data) {
+  let script = `if redis.call('get', '${lock_key}') == '${lock_id}' then
+    if redis.call('del', '${lock_key}') == 1 then
+      return redis.call('set', '${key}', '${data}', 'PX', 300000)
+    else
+      return -2
+    end
+  else
+    return -1
+  end`
+  // console.log(script)
+  let result = await redis_client.evalAsync(script, 0)
+  // console.log('unlock-: '+result)
+  return result
+}
+
+// 入口
+async function main(model, redis_client, id, flag) {
+  
+  // 资源在redis中的标识符
+  let key = flag+'-'+id
+  
+  // 资源锁key，通过这个字段是否为null判断资源是否上锁
+  let lock_key = flag+'-lock-'+id
+  
+  // 锁id，防止解除不属于自己的锁
+  let lock_id = Math.random()
+  
+  // 查询redis
+  let result_redis = await query_redis(redis_client, key)
+  if (result_redis !== null) {
+    // 从缓存中获取到了数据
+    // 刷新资源过期时间
+    reset_redis(redis_client, key)
+    return new Promise( resolve => {
+      resolve({
+        code: 0,
+        status: 200,
+        from: 'cache',
+        message: 'complete',
+        data: JSON.parse(result_redis)
+      })
+    })
+  }
+  // 给资源上锁
+  let lock = await redis_lock(redis_client, lock_key, lock_id)
+  if (lock === false) {
+    // 未成功上锁
+    // 自旋等待
+    await sleep(50)
+    return await main(model, redis_client, id, flag)
+  }
+  // 成功上锁
+  let result_mongo = await query_mongo(model, id)
+  if (result_mongo === null) {
+    // 资源不存在
+    ctx.throw(404, 'resourse not found')
+  }
+  // 解除锁同时刷新redis数据，此操作只能解除自己设置的锁
+  let data = JSON.stringify(result_mongo)
+  await redis_unlock(redis_client, lock_key, lock_id, key, data)
+  // 返回数据
+  return new Promise( resolve => {
+    resolve({
+      code: 0,
+      status: 200,
+      from: 'db',
+      message: 'complete',
+      data: result_mongo
+    })
+  })
+}
+
+module.exports = main
