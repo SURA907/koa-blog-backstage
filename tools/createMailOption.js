94f74692c0e9d1377a197385f003d70be8e48196
sura <sura907@hotmail.com>
2018-12-29 16:47:15 +0800

    change mail template

--- tools/createMailOption.js
+++ tools/createMailOption.js
@@ -9,19 +9,23 @@ const options = {
       from: '"SURA论坛邮件" <sura907@163.com>', // 发件人
       to: to, // 收件人
       subject: 'SURA论坛-用户注册系统', // 主题
-      text: `您好，欢迎成为SURA论坛用户。您的验证码为：${verification_code}，
-      验证码有效期5分钟，请不要向任何人泄露您的验证码.
-      若您未进行相关操作，请忽略本邮件.`
+      text: `您好，欢迎成为SURA论坛用户
+      您的验证码为：${verification_code}
+      验证码有效期5分钟
+      请不要向任何人泄露您的验证码
+      若您未进行相关操作，请忽略本邮件`
     }
   },
   // 用户修改密码邮件格式
-  change_password(to, verification_code) {
+  change_password(to, username, verification_code) {
     return {
       from: '"SURA论坛邮件" <sura907@163.com>',
       to: to,
       subject: 'SURA论坛-密码修改系统',
-      text: `您好，您正在修改用户密码。验证码为：${verification_code}，
-      验证码有效期5分钟，请不要向任何人泄露您的验证码.
+      text: `您好，您正在修改用户名为：${username} 的登录密码
+      此次操作验证码为：${verification_code}
+      验证码有效期5分钟
+      请不要向任何人泄露您的验证码
       若您未进行相关操作，请忽略本邮件`
     }
   }
